6e083883f6c45979c974bddae4fedf2819df5683
Hodan <you@example.com>
2022-01-06 20:21:03 -0500

    added properties and methods

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const inquirer = require("inquirer");
+
+const managerQuest = [
+  { type: "input", name: "name", message: "What is the managers name?" },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "What is the manager's name?",
+  },
+];
