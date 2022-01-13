cf8cf147b83204236753addd9ce7e5f393207a3c
Hodan <you@example.com>
2022-01-13 06:44:29 -0500

    created intern constructor and completed testing

--- __test__ /Manager.test.js
+++ __test__ /Manager.test.js
@@ -1,8 +1,28 @@
 const Manager = require("../lib/Manager");
 
-const manager = new Manager("joe", 222, "test@test.com", 1054);
+const joe = new Manager("joe", 222, "test@test.com", 1054);
 
-it("should have property of office number and getRole method", () => {
-  expect(typeof manager.officeNumber).not.toBeNaN();
-  expect(manager.getRole).toHaveReturnedWith("Manager");
+//Testing for properties
+it("should have property of name, id, email, and office number", () => {
+  expect(joe.name).toEqual("joe");
+  expect(joe.id).not.toBeNaN();
+  expect(typeof joe.email).toBe("string");
+});
+
+it("should return the name property of the object", () => {
+  expect(joe.getName()).toEqual("joe");
+});
+
+//Testing of methods
+it("should return the id property", () => {
+  // console.log(typeof jeff.getID());
+  expect(typeof joe.getID()).toBe("number");
+});
+
+it("should return the email property", () => {
+  expect(typeof joe.getEmail()).toBe("string");
+});
+
+it("should return manager", () => {
+  expect(joe.getRole()).toEqual("Manager");
 });
